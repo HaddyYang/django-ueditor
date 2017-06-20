76ae079c9dbe5053a1adf07928c7d520028628ef
HaddyYang <624644925@qq.com>
2017-06-20 22:32:04 +0800

    兼容Django 1.11

--- Demo/ueditor/urls.py
+++ Demo/ueditor/urls.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 from django.conf.urls import include, url
+from django.views.static import serve
 from .controller import handler
 import os
 
@@ -7,6 +8,5 @@ ueditor_path = os.path.join(os.path.dirname(__file__), "UE")
 
 urlpatterns = [
     url(r'^$', handler),
-    url( r'^UE/(?P<path>.*)$', 'django.views.static.serve',
-        { 'document_root': ueditor_path }),
+    url( r'^UE/(?P<path>.*)$', serve, {'document_root': ueditor_path}),
 ]
